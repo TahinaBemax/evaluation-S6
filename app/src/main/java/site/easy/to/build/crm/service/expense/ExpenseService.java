8e97d4a656c9395114f53c2424d017a93fe1d201
tahinaBemax <tahina.nambinintsoa@passerellesnumeriques.org>
2025-03-22 12:02:20 +0300

    creation des fonctions crud terminé

---
+++ app/src/main/java/site/easy/to/build/crm/service/expense/ExpenseService.java
@@ -0,0 +1,14 @@
+package site.easy.to.build.crm.service.expense;
+
+
+import site.easy.to.build.crm.entity.Budget;
+import site.easy.to.build.crm.entity.Expense;
+
+import java.util.List;
+
+public interface ExpenseService {
+    public List<Expense> findAll();
+    public Expense findById(Integer id);
+    public Expense save(Expense expense);
+    public void delete(Expense expense);
+}
